dd3ddcb641fd183504d04ec7f446dbc79fb2c61d
MihailHaev <khriokhriokh@icloud.com>
2019-05-08 23:20:32 +0300

    EsLint Fix for Travis

--- src/components/app/app.e2e.test.js
+++ src/components/app/app.e2e.test.js
@@ -1,7 +1,7 @@
 import React from "react";
 import {configure, mount} from "enzyme";
 import Adapter from "enzyme-adapter-react-16";
-import App from "./App";
+import App from "./app.jsx";
 
 configure({adapter: new Adapter()});
 
